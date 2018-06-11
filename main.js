2b19c7f7b49bd0125af0656f257a7ad5d213d42a
Bluice Zhen <reg@zhenlanbing.com>
2018-06-11 14:47:34 +0800

    屏蔽知乎专栏

---
+++ main.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.zhihu.com/
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+    function removeZhuanLan() {
+        for (let i = window.cardListLength; i < eCardList.children.length; i++) {
+            let eMetaList = eCardList.children[i].getElementsByTagName('meta');
+            for (let j = 0; j < eMetaList.length; j++) {
+                if (eMetaList[j].getAttribute('itemprop') === 'url' && eMetaList[j].getAttribute('content').match('zhuanlan')) {
+                    eCardList.children[i].hidden = true;
+                }
+            }
+        }
+    }
+
+    window.cardListLength = 0;
+    let eCardList = document.getElementsByClassName("TopstoryMain")[0].children[0];
+
+
+    eCardList.addEventListener('DOMNodeInserted', function () {
+        if (window.cardListLength !== eCardList.children.length) {
+            removeZhuanLan();
+        }
+    })
+})();
