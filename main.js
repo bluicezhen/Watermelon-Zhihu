2a2b0ee5204698b47b13c0df6289afbaa17e55f9
Bluice Zhen <reg@zhenlanbing.com>
2018-06-11 15:23:50 +0800

    修复问题

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         Watermelon Zhihu
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @version      0.1.1
+// @description  一个清净的知乎
+// @author       Bluice Zhen
 // @match        https://www.zhihu.com/
 // @grant        none
 // ==/UserScript==
@@ -11,19 +11,24 @@
 (function () {
     'use strict';
     function removeZhuanLan() {
-        for (let i = window.cardListLength; i < eCardList.children.length; i++) {
-            let eMetaList = eCardList.children[i].getElementsByTagName('meta');
-            for (let j = 0; j < eMetaList.length; j++) {
-                if (eMetaList[j].getAttribute('itemprop') === 'url' && eMetaList[j].getAttribute('content').match('zhuanlan')) {
-                    eCardList.children[i].hidden = true;
+        for (let i = 0; i < eCardList.children.length; i++) {
+            if (eCardList.children[i].hidden == false) {
+                let eMetaList = eCardList.children[i].getElementsByTagName('meta');
+                for (let j = 0; j < eMetaList.length; j++) {
+                    if (eMetaList[j].getAttribute('itemprop') === 'url' && eMetaList[j].getAttribute('content').match('zhuanlan')) {
+                        eCardList.children[i].hidden = true;
+                        console.debug('【Watermelon Zhihu】 屏蔽专栏文章：', eMetaList[j].getAttribute('content'));
+                        break;
+                    }
                 }
             }
         }
     }
 
-    window.cardListLength = 0;
+    console.debug('【Watermelon Zhihu】 开始运行');
     let eCardList = document.getElementsByClassName("TopstoryMain")[0].children[0];
 
+    removeZhuanLan();
     eCardList.addEventListener('DOMNodeInserted', function () {
         if (window.cardListLength !== eCardList.children.length) {
             removeZhuanLan();
